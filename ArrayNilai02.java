08bce24f7cb0c088c3eff9b7b59131a18b4552c2
Yuanzth <adityayuhanda3001@gmail.com>
2023-10-28 15:17:51 +0700

    Percobaan 2 Done

--- ArrayNilai02.java
+++ ArrayNilai02.java
@@ -5,14 +5,21 @@ public class ArrayNilai02 {
         Scanner input = new Scanner(System.in);
         int[] nilaiAkhir = new int[10];
 
-        for (int i = 0 ; i < 10 ; i++)
+        for (int i = 0 ; i < nilaiAkhir.length ; i++)
         {
             System.out.print("Masukkan nilai akhir ke-"+i+" : ");
             nilaiAkhir[i] = input.nextInt();
         }
-        for (int i = 0; i < 10 ; i++)
+        for (int i = 0; i < nilaiAkhir.length ; i++)
         {
-            System.out.println("Nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
+            if (nilaiAkhir[i] > 70)
+            {
+                System.out.println("Mahasiswa ke-"+i+" lulus!");
+            }
+            else 
+            {
+                System.out.println("Mahasiswa ke-"+i+" tidak lulus.");
+            }
         }
     }
 }
