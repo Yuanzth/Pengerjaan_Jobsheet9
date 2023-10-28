adf5e3f6a67be59893e5328d60834292894191fa
Yuanzth <adityayuhanda3001@gmail.com>
2023-10-28 13:52:17 +0700

    Percobaan 1 Done

--- ArrayBilangan02.java
+++ ArrayBilangan02.java
@@ -1,19 +1,12 @@
-import java.util.Scanner;
-public class ArrayBilangan02 {
-public static void main(String[] args) {
+    import java.util.Scanner;
+    public class ArrayBilangan02 {
+        public static void main(String[] args) {
+            int[] bil = {5, 13, -7, 17};
 
-    int[] bil = new int[4];
-    bil[0] = 5;
-    bil[1] = 13;
-    bil[2] = -7;
-    bil[3] = 17;
+            for (int i = 0; i <= 4; i++)
+            {
+                System.out.println(bil[i]);
+            }
 
-    System.out.println(bil[0]);
-    System.out.println(bil[1]);
-    System.out.println(bil[2]);
-    System.out.println(bil[3]);
-
-    
-}
-    
-}
+        }
+    }
